6ca24dba120619e6102b5e95e1a92d5b3d3bbed1
Stephanee Hayden <stephaneehayden@gmail.com>
2023-02-05 13:55:20 +1000

    get and post calls working:

--- routes/notes.js
+++ routes/notes.js
@@ -1,14 +1,34 @@
-const path = require('path');
 const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
 
-// GET Route for homepage
-notes.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+//GET route for retrieving all notes from db.json file
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
 
-// GET Route for notes page
-notes.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+// POST Route for a new NOTES
+notes.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        noteID: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`New note added successfully 🚀`);
+    } else {
+      res.error('Error in adding new note');
+    }
+  });
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static("public"));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// Wildcard route to direct users to the index page
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 // GET Route for notes page
+//TODO: GET /notes should return the notes.html file.
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Wildcard route to direct users to the index page
+//TODO: GET * should return the index.html file.
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
