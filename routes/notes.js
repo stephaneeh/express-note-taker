248e1ae4d932d71676ac89a2507f3be51d6875e6
Stephanee Hayden <stephaneehayden@gmail.com>
2023-02-08 19:44:14 +1000

    all API calls working via insomnia

--- routes/notes.js
+++ routes/notes.js
@@ -1,29 +1,48 @@
 const notes = require('express').Router();
+const uuid = require('./../helpers/uuid');
 const fs = require('fs');
-const uuid = require('../helpers/uuid');
 const {
-  readFromFile,
-  readAndAppend,
-  writeToFile,
-} = require('../helpers/fsUtils');
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+} = require('./../helpers/fsUtils');
+
 
 //GET route for retrieving all notes from db.json file
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// GET Route for a specific note
+notes.get('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+      const result = json.filter((note) => note.note_id === noteId);
+      return result.length > 0
+          ? res.json(result)
+          : res.json('No note with that ID');
+      });
+  });
+    
+// DELETE Route for a specific note
+notes.delete('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.note_id !== noteId);
+  
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+  
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
   });
 
-//GET route for retrieving all notes from db.json file
-notes.get('/:note_id', (req, res) => {
-  const noteID = req.params.note_id;
-  readFromFile('./db/db.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-      const result = json.filter((note) => note.note_id === noteID);
-      return result.length > 0
-        ? res.json(result)
-        : res.json('No tip with that ID');
-    });
-});
 
 
 // POST Route for a new note
@@ -31,52 +50,50 @@ notes.post('/', (req, res) => {
   console.log(req.body);
   // Log that a POST request was received
   console.info(`${req.method} request received to add a note`);
-
+  
   // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
-  if (req.body) {
+    if (req.body) {
       const newNote = {
-        title,
-        text,
-        note_ID: uuid(),
-      };
-
-    // Obtain existing notes
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          // Convert string into JSON object
-          const parsedNotes = JSON.parse(data);
-  
-          // Add new note
-          parsedNotes.push(newNote);
-  
-          // Write updated notes back to the file
-          fs.writeFile(
-            './db/db.json',
-            JSON.stringify(parsedNotes, null, 4),
-            (writeErr) =>
-              writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully saved note!')
-          );
-        }
-      });
-  
-      const response = {
-        status: 'success',
-        body: newNote,
+      title,
+      text,
+      note_id: uuid(),
       };
   
-      console.log(response);
-      res.status(201).json(response);
-  } else {
-    res.status(500).json('Error in saving note');
-  }
-});
-
-
+        // Obtain existing notes
+        readFromFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+            } else {
+              // Convert string into JSON object
+              const parsedNotes = JSON.parse(data);
+      
+              // Add new note
+              parsedNotes.push(newNote);
+      
+              // Write updated notes back to the file
+              fs.writeFile(
+                './db/db.json',
+                JSON.stringify(parsedNotes, null, 4),
+                (writeErr) =>
+                  writeErr
+                    ? console.error(writeErr)
+                    : console.info('Successfully saved note!')
+              );
+            }
+          });
+      
+          const response = {
+            status: 'success',
+            body: newNote,
+          };
+      
+          console.log(response);
+          res.status(201).json(response);
+      } else {
+        res.status(500).json('Error in saving note');
+      }
+  });
 
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -14,17 +14,15 @@ app.use(express.static("public"));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/index.html'))
 );
 
 // GET Route for notes page
-//TODO: GET /notes should return the notes.html file.
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // Wildcard route to direct users to the index page
-//TODO: GET * should return the index.html file.
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
